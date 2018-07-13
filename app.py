fe837792023d6a6278587261d89f461b289c6b32
Kyle Hines <kylehines@Kyles-MacBook-Air.local>
2018-07-13 16:46:52 -0600

    commented out old code removed

--- app.py
+++ app.py
@@ -8,13 +8,6 @@ app = Flask(__name__)
 signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
 @app.route("/")
-"""
-def generate_buzz():
-    page = '<html><body><h1>'
-    page += generator.generate_buzz()
-    page += '</h1></body></html>'
-    return page
-"""
 def hello():
 	return render_template('index.html')
 
